e3a79b8b4aa0379604467ebe530eb1c65d113492
Karl Damkjær Hansen <kdh@es.aau.dk>
2019-04-01 15:30:05 +0200

    Green button also works

--- twobutton.cpp
+++ twobutton.cpp
@@ -2,11 +2,15 @@
 
 int led_red_pin = 11;
 int button_1_pin = 3;
+int led_green_pin = 10;
+int button_2_pin = 2;
 
 void setup()
 {
 	pinMode(led_red_pin, OUTPUT);
   pinMode(button_1_pin, INPUT_PULLUP);
+  pinMode(led_green_pin, OUTPUT);
+  pinMode(button_2_pin, INPUT_PULLUP);
 }
 
 void loop()
@@ -15,5 +19,10 @@ void loop()
 	  digitalWrite(led_red_pin, HIGH);
   } else {
     digitalWrite(led_red_pin, LOW);
+  }
+  if (digitalRead(button_2_pin) == LOW) {
+	  digitalWrite(led_green_pin, HIGH);
+  } else {
+    digitalWrite(led_green_pin, LOW);
   }
 }
