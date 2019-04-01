24127f7ad4655a16b4707f6bf6380f0ca56c8f52
Karl Damkjær Hansen <kdh@es.aau.dk>
2019-04-01 15:27:13 +0200

    Button turns on red LED

--- twobutton.cpp
+++ twobutton.cpp
@@ -1,13 +1,19 @@
 #include "Arduino.h"
 
 int led_red_pin = 11;
+int button_1_pin = 3;
 
 void setup()
 {
 	pinMode(led_red_pin, OUTPUT);
+  pinMode(button_1_pin, INPUT_PULLUP);
 }
 
 void loop()
 {
-	digitalWrite(led_red_pin, HIGH);
+  if (digitalRead(button_1_pin) == LOW) {
+	  digitalWrite(led_red_pin, HIGH);
+  } else {
+    digitalWrite(led_red_pin, LOW);
+  }
 }
